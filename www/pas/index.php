ce7699cd69bd702821f335a89653fc607b566e1a
youte62 <jeremylheureux@gmailcom>
2017-10-11 06:50:15 +0200

    add jetaim.eu/pas

--- www/pas/index.php
+++ www/pas/index.php
@@ -10,7 +10,7 @@
 <meta name="keywords" content="amour, sentiment, je t'aime, prénom" />
 <meta name="language" content="french francais" />
 
-<title>Je voulais te dire que je t'aime</title>
+<title>Je voulais te dire que je ne t'aime pas</title>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
